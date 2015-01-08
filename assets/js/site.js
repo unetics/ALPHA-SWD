bf4558177facf6504dc925fa17266f118e9d4765
Mitchell Bray <mitchellbray@Mitchells-MBP.BigPond>
2015-01-08 13:43:17 +1100

    force bottom js

--- assets/js/site.js
+++ assets/js/site.js
@@ -4,3 +4,23 @@
 //@codekit-prepend "chosen.jquery.js"
 //@codekit-prepend "responsivemobilemenu.js"
 //@codekit-prepend "jquery.slabtext.js"
+
+
+(function($) {    
+	
+		$(document).ready(function() {
+			bottom();
+			$( window ).resize(function() {
+		       if ($(window).width() > 960) {
+				   bottom();
+				}
+				$('.align-bottom ').css( "paddingTop", "" );
+			});
+			function bottom() {
+				var height = parseInt($('.align-bottom').height());
+				var offset = $('.align-bottom').parents(".panel-row-style").height() - height; 
+				$('.align-bottom ').css( "paddingTop", offset );
+			}
+		});
+		
+})( jQuery );	
